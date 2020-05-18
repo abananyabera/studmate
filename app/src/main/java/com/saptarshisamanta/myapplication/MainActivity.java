c5862afe7999d08a59f6d559ba47f5ff82a1247f
saptarshi <36296989+Saptarshisamata@users.noreply.github.com>
2020-05-18 21:54:03 +0530

    initial commit

--- app/src/main/java/com/saptarshisamanta/myapplication/MainActivity.java
+++ app/src/main/java/com/saptarshisamanta/myapplication/MainActivity.java
@@ -4,15 +4,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.saptarshisamanta.myapplication.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    int sem;
+    String stream;
+    String CSE = "cse";
     ActivityMainBinding activityMainBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         activityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+        activityMainBinding.cse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stream = CSE;
+            }
+        });
+        activityMainBinding.first.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sem = 1;
+            }
+        });
+    }
+    public void choose(View view){
+        if (stream ==  CSE){
+            if (sem == 1){
+                //code for intent
+            }
+        }
     }
 }
